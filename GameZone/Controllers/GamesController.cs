d6fd56f9ea8e9f849318892a588a78162c953818
ammarnas <ammarnasser.contact@gmail.com>
2026-06-14 16:27:45 +0300

    08. Add Games Controller

---
+++ GameZone/Controllers/GamesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameZone.Controllers;
+
+public class GamesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
